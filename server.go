a6b863841dfbbab536ee672977d9f2906b6bd6db
Jason Connell <jconnell@delphicdigital.com>
2014-12-15 11:06:01 -0500

    Update router to use gorilla tools mux

--- server.go
+++ server.go
@@ -1,11 +1,16 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 	"log"
+	"github.com/gorilla/mux"
 )
 
-type Handler func(http.ResponseWriter, *http.Request)
+type Handler struct {
+	pattern string
+	handler func(http.ResponseWriter, *http.Request)
+}
 
 type Site struct {
 	Domain string
@@ -14,11 +19,26 @@ type Site struct {
 	TemplateFiles []string
 }
 
+func (site *Site) AddHandler(pattern string, handleFunc func(http.ResponseWriter, *http.Request)) {
+	var h = &Handler{ pattern: pattern, handler: handleFunc}
+	site.Handlers = append(site.Handlers, *h)
+}
+
+
 func Start(site Site){
+	mux := mux.NewRouter()
+
+	for _, h := range site.Handlers {
+		mux.HandleFunc(h.pattern, h.handler)
+	}
+
 	server := &http.Server{
-		Addr: site.Domain + ":"  + string(site.Port),
+		Addr: site.Domain + ":"  + fmt.Sprint(site.Port),
+		Handler: mux,
 	}
 
 
+	fmt.Println("Starting server ", server.Addr)
 	log.Fatal(server.ListenAndServe())
+	fmt.Println("Server started")
 }
